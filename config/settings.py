252fe90e9f84d0f12698953d1f9d8d08b8fb2e91
chodry <mchodrine@gmail.com>
2022-08-12 16:59:51 +0300

    third commit

--- config/settings.py
+++ config/settings.py
@@ -141,8 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'audio')
 
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = '/login'
-LOGIN_URL = '/login'
+LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = '/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
