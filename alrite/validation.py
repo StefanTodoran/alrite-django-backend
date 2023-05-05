4a90d8c3dded8c03e7362fe938c90f7374fc1a31
StefanTodoran <stefan.alex4@gmail.com>
2023-05-05 12:52:56 -0700

    more work on validation

--- alrite/validation.py
+++ alrite/validation.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import copy
 
 needsValueID = [
   "MultipleChoice",
@@ -13,21 +14,21 @@ def missingErrorMessage(type: str, prop: str) -> str:
   else:
     return f"{type.capitalize()} requires {prop} property!"
 
-# Verifies that the given property is unique given a list
-# of possible values. Beware: modifies the provided list!
-def ensureUniqueID(type: str, obj, prop: str, IDs: list):
+# Verifies that the given ID property is unique given a set
+# of possible ID values. Beware: modifies the provided set!
+def ensureUniqueID(type: str, obj, prop: str, IDs: set, required: bool):
   if prop in obj:
     value = obj[prop]
     if value in IDs:
-      IDs.remove(value)
+      IDs.discard(value)
     else:
       obj[prop] = f"Duplicate {prop} detected!"
-  else:
+  elif required:
     obj[prop] = missingErrorMessage(type, prop)
 
 # Verifies the the property on the given object is an actual
 # identifier in use somewhere. Supports optional properties.
-def isValidID(type: str, obj, prop: str, IDs: list, required: bool):
+def isValidID(type: str, obj, prop: str, IDs: set, required: bool):
   if prop in obj:
     value = obj[prop]
     if value not in IDs:
@@ -35,63 +36,84 @@ def isValidID(type: str, obj, prop: str, IDs: list, required: bool):
   elif required:
     obj[prop] = missingErrorMessage(type, prop)
 
-def validatePageObj(page, pageIDs, unusedPageIDs):
-  ensureUniqueID("page", page, "pageID", unusedPageIDs)
+# =========================== #
+# PAGE & COMPONENT VALIDATION #
 
-  isValidID("page", page, "defaultLink", pageIDs, True)
-  if page["defaultLink"] == page["pageID"]:
-    page["defaultLink"] = "Page should not link to itself!"
+def validatePageObj(originalPage, validatedPage, pageIDs: set, unusedPageIDs: set):
+  ensureUniqueID("page", validatedPage, "pageID", unusedPageIDs, True)
+  isValidID("page", validatedPage, "defaultLink", pageIDs, True)
+
+  if originalPage["defaultLink"] == originalPage["pageID"]:
+    validatedPage["defaultLink"] = "Page should not link to itself!"
   
-  if "title" not in page:
-    page["title"] = missingErrorMessage("page", "title")
+  if "title" not in originalPage:
+    validatedPage["title"] = missingErrorMessage("page", "title")
   
-  if "content" not in page:
-    page["content"] = missingErrorMessage("page", "content")
+  if "content" not in originalPage:
+    validatedPage["content"] = missingErrorMessage("page", "content")
+
+def validateComponentObj(originalComponent, validatedComponent, valueIDs: set, unusedValueIDs: set):
+  componentType = originalComponent["component"]
+  ensureUniqueID(componentType, validatedComponent, "valueID", unusedValueIDs, componentType in needsValueID)
 
 # Takes a workflow object and returns a tuple with two contents.
-# The first is a list of all page IDs and the second a list of all
-# value IDs. Also takes a boolean doValidation, signaling whether to
-# modify the workflow object with errors if identifiers are missing.
-def getAllIdentifiers(workflow, doValidation):
-  pageIDs = []
-  valueIDs = []
+# The first is a set of all page IDs and the second a set of all
+# value IDs. Also takes an optional artifact parameter, this is an
+# object which stores validation errors. 
+def getAllIdentifiers(workflow, artifact):
+  pageIDs = set()
+  valueIDs = set()
   
   for pageIndex in range(len(workflow["pages"])):
     page = workflow["pages"][pageIndex]
 
     if "pageID" in page:
-      pageIDs.append(page["pageID"])
+      pageIDs.add(page["pageID"])
     else:
-      page["pageID"] = missingErrorMessage("page", "pageID")
+      artifact["pageID"] = missingErrorMessage("page", "pageID")
     
     for componentIndex in range(len(page["content"])):
       component = page["content"][componentIndex]
 
       if "valueID" in component:
-        valueIDs.append(component["valueID"])
+        valueIDs.add(component["valueID"])
       elif component["component"] in needsValueID:
-        component["valueID"] = missingErrorMessage("component", "valueID")
+        workflow["content"][componentIndex]["valueID"] = missingErrorMessage("component", "valueID")
   
   return pageIDs, valueIDs
+
+# Validates the given workflow, returning an json-style object where
+# properties map to error messages. Does not mutate the input workflow.
+def validateWorkflow(workflow):
+  artifact = copy.deepcopy(workflow)
+  
+  pageIDs, valueIDs = getAllIdentifiers(workflow, True)
+
+  unusedPageIDs = copy.copy(pageIDs)
+  unusedValueIDs = copy.copy(valueIDs)
+
+  for pageIndex in range(len(workflow["pages"])):
+    originalPage = workflow["pages"][pageIndex]
+    validatedPage = artifact["pages"][pageIndex]
+
+    validatePageObj(originalPage, validatedPage, pageIDs, unusedPageIDs)
+
+    for componentIndex in range(len(originalPage["content"])):
+      originalComponent = originalPage["content"][componentIndex]
+      validatedComponent = validatedPage["content"][componentIndex]
+
+      validateComponentObj(originalComponent, validatedComponent, valueIDs, unusedValueIDs)
+
+  return artifact
+
+# ============== #
+# VALIDATION CLI #
 
 def readWorkflowFromFile(path: str):
   with open(path, 'r') as file:
     workflow = json.load(file)
 
   print(f"Successfully loaded workflow '{workflow['name']}'\n")
-  return workflow
-
-def validateWorkflow(workflow):
-  pageIDs, valueIDs = getAllIdentifiers(workflow, True)
-
-  unusedPageIDs = list(pageIDs)
-  unusedValueIDs = list(valueIDs)
-
-  for index in range(len(workflow["pages"])):
-    page = workflow["pages"][index]
-
-    validatePageObj(page, pageIDs, unusedPageIDs)
-
   return workflow
 
 if __name__ == "__main__":
@@ -101,7 +123,8 @@ if __name__ == "__main__":
     sys.exit()
   else:
     print(f"Reading workflow from '{sys.argv[1]}'")
-    artifact = readWorkflowFromFile(sys.argv[1])
+    data = readWorkflowFromFile(sys.argv[1])
+    artifact = validateWorkflow(data)
     
     print(f"Writing validation artifact to '{sys.argv[2]}'")
     with open(sys.argv[2], 'w') as file:
